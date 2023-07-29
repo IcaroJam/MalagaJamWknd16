7b12cd549e9c115ff40ac275e7c8f52d48e5c7ee
senwinari <onextthenitromancer@gmail.com>
2023-07-30 00:07:58 +0200

    Bad ending on enough hits.

--- Assets/Scripts/PlanetCounter.cs
+++ Assets/Scripts/PlanetCounter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlanetCounter : MonoBehaviour
 {
@@ -23,8 +24,10 @@ public class PlanetCounter : MonoBehaviour
 	{
 		_hits++;
 
-		if (_hits >= _hitsToEnd)
+		if (_hits >= _hitsToEnd) {
 			Debug.Log("EARTH'S DEAD!");
+			SceneManager.LoadScene("BadEnding");
+		}
 	}
 
 	public uint Hits => _hits;
