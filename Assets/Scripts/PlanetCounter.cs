8723dea71b59d3610f9176b820442f6c392ce9b4
senwinari <onextthenitromancer@gmail.com>
2023-07-29 19:26:47 +0200

    Hit counter.

---
+++ Assets/Scripts/PlanetCounter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetCounter : MonoBehaviour
+{
+	private uint					_hits = 0;
+	[SerializeField] private float	_hitsToEnd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		_hits++;
+
+		if (_hits >= _hitsToEnd)
+			Debug.Log("EARTH'S DEAD!");
+	}
+}
