536a33ec2c6fd30fd5cfc5e50506ef072b781e21
Nol3 <alejandrocp_952@hotmail.com>
2023-07-29 12:26:40 +0200

    asteroides
    Carlos's things

---
+++ Assets/Scripts/Enemy_Moves/enemy1_movement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy1_movement : MonoBehaviour
+{
+    public GameObject Player;
+    public float speed;
+    private float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Planet");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, Player.transform.position);
+        Vector2 direction = Player.transform.position - transform.position;
+        transform.position = Vector2.MoveTowards(this.transform.position, Player.transform.position, speed * Time.deltaTime);
+    }
+}
