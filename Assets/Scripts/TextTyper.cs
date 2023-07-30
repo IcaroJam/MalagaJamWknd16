6bc51863885520d0fa4bd40e0fbf93788d195207
senwinari <onextthenitromancer@gmail.com>
2023-07-30 10:04:33 +0200

    Lore text beginnings.

---
+++ Assets/Scripts/TextTyper.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextTyper : MonoBehaviour
+{
+	private TMP_Text				_txtRef;
+	private string					_textToPrint;
+	[SerializeField] private float	_printTime;
+
+	void Awake()
+	{
+		_txtRef = GetComponent<TMP_Text>();
+		_textToPrint = _txtRef.text;
+		_txtRef.text = string.Empty;
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(printIt());
+    }
+
+	IEnumerator printIt()
+	{
+		for (int i = 0; i < _textToPrint.Length; i++) {
+			_txtRef.text += _textToPrint[i];
+			yield return new WaitForSeconds(_printTime);
+		}
+		yield return null;
+	}
+}
